3b8ac979560550c4153bddeefc822247308a620f
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2021-02-28 15:57:11 +0200

    hjhj

--- Churilova_Luniakina_AIS.pl
+++ Churilova_Luniakina_AIS.pl
@@ -75,8 +75,8 @@ worker(39, 68000, pib("Andii", "Pukhliar", "Oleksiyovych"), phones("0631371110")
 % Фільм(назва_фільму(PK), дата_випуску, тривалість[], бюджет, номер_знімальної_групи(FK), номер_групи_монтажерів(FK))
 movie("The First Star", date(2019,03,21), [120,32], 1023010, 1, 1).
 movie("Candy Bar", date(2020,11,13), [183], 2323521, 2, 1).
-movie("See you tomorrow", date(2019,05,04), [168,158,54], 3, 3).
-movie("The Old Mill", date(2017,10,15), [183,32], 4, 1).
+movie("See you tomorrow", date(2019,05,04), [168,158,54], 2311900, 3, 3).
+movie("The Old Mill", date(2017,10,15), [183,32], 34292880, 4, 1).
 
 % Багатозначний атрибут жанр [1..n]. genres(код_жанру(PK), назва_жанру)
 genres(1, "Fiction").
@@ -214,9 +214,9 @@ actors_age_of_film(Movie,Actors_age):- movie(Movie, _, _, _, Id_filmCrew, _), fi
 
 
 % В яких фільмах знімався даний актор
-% films_of_actor(+прізвище актора,-фільми).
+% films_of_actor(+прізвище актора,-фільми). ??????????????????
 
-films_of_actor(Surname, Movie):- worker_filmCrew(filmCrew_number, ID_actor, _), movie(Movie, _, _, _, filmCrew_number, _), worker(ID_actor, _, pib(_,Surname,_), _, _, _, _, _, _).
+films_of_actor(Surname, Movie):- worker(ID_actor, _, pib(_,Surname,_), _, _, _, _, _, _), worker_filmCrew(filmCrew_number, ID_actor, _), movie(Movie, _, _, _, filmCrew_number, _).
 
 
 
