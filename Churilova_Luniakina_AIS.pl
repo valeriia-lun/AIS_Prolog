ee0216d21827a26743dfec12c2bb9f8a5196b08b
anikachurilova <61321254+anikachurilova@users.noreply.github.com>
2021-02-28 13:15:43 +0200

    Update Churilova_Luniakina_AIS.pl
    add film crew workers

---
+++ Churilova_Luniakina_AIS.pl
@@ -0,0 +1,55 @@
+% правило №1: Співробітний знімальної групи(табельний номер(PK), зарплата, піб(імя, прізвище, по-батькові), телефон[], домашня адреса, працює з, дата народження, назва посади, стать)
+worker(1, 15000, pib("Sofiya", "Makarenko", "Olegivna"), ["0631111111","0932222222", "0933333333","0934444444"], "Kyiv, Maryny Tsvetaivoi, 15", date(2018-07-23), date(1990-12-12), "actor", "female").
+worker(2, 26000, pib("Anna", "Voronina", "Oleksandrivna"), ["0631115111","0932228222", "0933833333"], "Kyiv, Anny Akhmatovoi, 86a", date(2020-01-15), date(1995-02-21), "actor", "female").
+worker(3, 18000, pib("Oleg", "Maksymenko", "Volodymyrovych"), ["0631315111"], "Kyiv, Onore de Balsaka, 90", date(2016-02-11), date(1980-10-01), "actor", "male").
+worker(4, 11000, pib("Maksym", "Romanenko", "Volodymyrovych"), ["0631315011","0932328022"], "Kyiv, Antonovycha, 11b", date(2018-03-13), date(1982-11-02), "actor", "male").
+worker(5, 33000, pib("Vera", "Badun", "Sergiivna"), ["0631315091","0932328092", "0933873093"], "Kyiv, Velyka Vasylkivska, 110", date(2017-10-19), date(1982-10-02), "actor", "female").
+worker(6, 30000, pib("Valerie", "Nikulchenko", "Andriivna"), ["0631315551","0932558192", "0933855093"], "Kyiv, Dmytrovicha, 9a", date(2010-02-02), date(1977-09-09), "actor", "female").
+worker(7, 29000, pib("Anton", "Leonov", "Fedorovych"), ["0631315231","0932552392"], "Kyiv, Vozna, 77", date(2011-12-02), date(1988-02-09), "actor", "male").
+worker(8, 22000, pib("Roman", "Falyush", "Dmytrovych"), ["0631315220"], "Kyiv, Pozniakovska, 17b", date(2014-08-08), date(1992-12-05), "actor", "male").
+worker(9, 23000, pib("Valerii", "Prokhorov", "Dmytrovych"), ["0631315220","0631234567"], "Kyiv, Volodymyrovska, 45b", date(2018-11-09), date(1989-10-25), "actor", "male").
+worker(10, 45000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "actor", "male").
+
+worker(11, 22000, pib("Volodymyr", "Dykhner", "Sergiyovych"), ["0631720220","0630907767"], "Kyiv, Sokurenko, 12", date(2017-11-09), date(1967-11-25), "driver", "male").
+worker(12, 22000, pib("Sergii", "Vasylenko", "Romanovych"), ["0631309020"], "Kyiv, Vynogradna, 90", date(2019-09-01), date(1971-11-29), "driver", "male").
+
+worker(13, 18000, pib("Oksana", "Doroshenko", "Romanovych"), ["0931077220","0631237767","0551200767"], "Kyiv, Voronova, 80", date(2020-11-01), date(1970-02-15), "cook", "female").
+worker(14, 19000, pib("Alina", "Rezolovna", "Andriivna"), ["0631237007"], "Kyiv, Mayakovskogo, 81", date(2019-07-21), date(1975-07-20), "cook", "female").
+
+worker(15, 20000, pib("Oleksii", "Trandino", "Evgenovych"), ["06309007767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "loader", "male").
+worker(16, 21000, pib("Semen", "Kotsukon", "Artemovych"), ["0631300220","0631000220","0631000067"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "loader", "male").
+
+worker(17, 39000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "lighter", "male").
+worker(18, 40000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "lighter", "male").
+
+worker(19, 42000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "soundman", "male").
+worker(20, 44000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "soundman", "male").
+
+worker(21, 46000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "cleaner", "male").
+worker(22, 41000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "cleaner", "male").
+
+worker(23, 47000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "artist director", "male").
+worker(24, 48000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "artist director", "male").
+
+worker(25, 39000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "costume designer", "male").
+worker(26, 40000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "costume designer", "male").
+
+worker(27, 46000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "make-up artist", "male").
+worker(28, 47000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "make-up artist", "male").
+
+worker(29, 48000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "actor agent", "male").
+worker(30, 47000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "actor agent", "male").
+
+worker(31, 42000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "director", "male").
+worker(32, 43000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "director", "male").
+
+worker(33, 42000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "screenwriter", "male").
+worker(34, 41000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "screenwriter", "male").
+
+worker(35, 58000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "operator", "male").
+worker(36, 57000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "operator", "male").
+
+worker(37, 47000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "production administrator", "male").
+worker(38, 45000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "production administrator", "male").
+
+worker(39, 68000, pib("Mykhailo", "Dykhner", "Romanovych"), ["0631377220","0631237767"], "Kyiv, Saksaganskogo, 77b", date(2018-11-09), date(1988-10-25), "general producer", "male").
